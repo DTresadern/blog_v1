99483e7d7f7d11ece86adff74e02992ba30bf2f8
Dean Tresadern <deantresadern+gh@pm.me>
2023-04-30 12:03:57 +0100

    add local hosts for subdomains

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << 'articles.deantres.local'
+  config.hosts << 'blog.deantres.local'
+  config.hosts << 'toolbox.deantres.local'
+  config.hosts << 'admin.deantres.local'
+  config.hosts << 'deantres.local'
 end
